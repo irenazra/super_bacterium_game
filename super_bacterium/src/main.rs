e5c071dbc745d5dbae637686351a30614ef28245
irenazra <iaca2017@mymail.pomona.edu>
2021-02-09 02:17:17 +0300

    Debugged and and organized the functionality to read maps from files

--- super_bacterium/src/main.rs
+++ super_bacterium/src/main.rs
@@ -2,10 +2,11 @@ use rand::{thread_rng, Rng};
 use std::io;
 use std::collections::HashSet;
 use std::io::{Write};
+use std::str::FromStr;
+use std::fs;
 
 
-
-
+//Keeps track of the game state and simulates space
 #[derive (Clone)]
 struct Map {
     pub board: Vec<Vec<i32>>,
@@ -16,16 +17,15 @@ struct Map {
     pub col: i32
 }
 
-
-// 0 is empty 
-// 1 is the player
-// 2 is enemy
-// 3 is food
-// 4 exit
 impl Map {
+
+    //Prints the game board
+    //This is the main function that simulates the space of the game
     fn print_board(&self) {
+
         let width = self.board[0].len();
         let row_line = "-".repeat(width * 7);
+
 
         self.clone().board.into_iter().for_each(|it| {
             let mut row_string:String = "|".to_string();
@@ -50,6 +50,7 @@ impl Map {
         
     }
 
+    //Enables an object on the board to be moves somewhere else
     fn move_object(& mut self, new_row:i32, new_col:i32, row:i32, col:i32) {
         let object = self.board[row as usize][col as usize];
 
@@ -58,19 +59,23 @@ impl Map {
 
     }
 
+    // Determines whether the given position is valid
     fn is_valid_position(&self, row:i32, col:i32) -> bool{
         (row >= 0) & (row < self.board.len() as i32) & (col >= 0) & (col < self.board[0].len() as i32)
     }
 
+    //Randomly place specified amount and kind of elements on the board
     fn place_elements(& mut self, enemy_number : usize, food_number: usize,largest_row :usize , largest_col :usize ) {
         self.board[0][0] = 1;
         self.board[largest_row][largest_col] = 4;
+
+        //Keep track of the filled positions to avoid hits
         let mut filled_positions = HashSet::new();
         filled_positions.insert((0,0));
         filled_positions.insert((largest_row,largest_col));
     
+        //Randomly generate positions and place the specified amount of enemies
         let mut enemy_counter = enemy_number;
-    
         while enemy_counter > 0 {
             let random_row : usize = thread_rng().gen_range(0, self.board[0].len());
             let random_col : usize = thread_rng().gen_range(0, self.board[0].len());
@@ -80,11 +85,10 @@ impl Map {
                 self.board[random_row][random_col] = 2;
                 enemy_counter -= 1;
             }
-    
         }
     
+        //Randomly generate positions and place the specified amount of enemies
         let mut food_counter = food_number;
-    
         while food_counter > 0 {
             let r_row : usize = thread_rng().gen_range(0, self.board[0].len());
             let r_col : usize = thread_rng().gen_range(0, self.board[0].len());
@@ -94,47 +98,41 @@ impl Map {
                 self.board[r_row][r_col] = 3;
                 food_counter -= 1;
             }
-    
         }
-    
     }
 
-
-
-
-    
+    //Record the latest move to the records vector
     fn record_state(& mut self, row: i32, col : i32, the_move : i32, previous_object_num: i32) {
-        //0 up
-        //1 down
-        //2 right
-        //3 left
-
         //At position row,col the_move move was done
         let new_vec = vec![row,col,the_move,previous_object_num];
         self.records.push(new_vec);
     }
 
+    //Undo the latest move in the records vector
     fn rewind_one_move (& mut self) {
+        //Because we are rewinding, we need to give the energy back to the player
         self.energy += 1;
 
+        //Information about the last move
         let latest_move = self.records.pop().unwrap();
         let row = latest_move[0];
         let col = latest_move[1];
         let the_move = latest_move[2];
         let prev =latest_move[3];
 
+        //If we encountered an enemy in the last move
         if prev == 2 {
             self.damage -= 1;
         }
 
+        //If we found food in the last move
         if prev == 3 {
             self.energy -= 1;
         }
 
-
+        //Calculate the previous location of the player
         let mut current_row = row;
         let mut current_col = col;
-
         match the_move {
             0 => {current_row -= 1;},
             1 => {current_row += 1;},
@@ -143,19 +141,15 @@ impl Map {
             _ => println!("{}", "Something is wrong!"),
         }
 
+        //Place the player and the previous object at that location at their old positions
         self.board[row as usize][col as usize] = 1;
         self.board[current_row as usize][current_col as usize] = prev;
         self.col = col;
         self.row = row;
-
-
-
     }
-
-   
-
 }
 
+//Context free grammar which generates messages when the user moves to an empty space
 fn cfg_empty_print(){
     // Rules of the Grammar
     // S -> Helper, Positive Helper, Helper Positive
@@ -180,12 +174,10 @@ fn cfg_empty_print(){
     } else {
         let result = format!("{} {}",help, pos);
         print!("{}", result);
-
     }
-
-    
 }
 
+//Context free grammar which generates messages when the user moves to a space with food
 fn cfg_food_print(){
     // Rules of the Grammar
     // S -> Helper, Positive Helper, Helper Positive
@@ -210,12 +202,11 @@ fn cfg_food_print(){
     } else {
         let result = format!("{} {}",help, pos);
         print!("{}", result);
-
     }
-
-    
 }
 
+
+//Context free grammar which generates messages when the user moves to a space with an enemy
 fn cfg_enemy_print(){
     // Rules of the Grammar
     // S -> Helper, Negative Helper, Helper Negative
@@ -240,34 +231,26 @@ fn cfg_enemy_print(){
     } else {
         let result = format!("{} {}",help, neg);
         print!("{}", result);
-
     }
-
-    
 }
-
 
 
 fn main() {
     //----------------------------------------------
-    //*****CREATING A MAP THROUGH FILES STARTS*****
+    //    BEGIN: CREATING A MAP THROUGH A FILE
     //----------------------------------------------
     
-    // println!("In file {}", "test.txt");
-
     // let contents = fs::read_to_string("test.txt")
     //     .expect("Something went wrong reading the file");
 
-    // println!("With text:\n{}", contents);
-
+    // // Create an iterator 
     // let mut iterator = contents.split_whitespace();
 
+    // // Extract information from the file
     // let num_rows:i32 = FromStr::from_str(&iterator.next().unwrap()).unwrap();
     // let num_cols:i32 = FromStr::from_str(&iterator.next().unwrap()).unwrap();
 
-    
-
-    // let mut initial_board = vec![(vec![0 as i32;num_cols as usize]);num_rows as usize];
+    // let mut initial_board = vec![vec![0 as i32;num_cols as usize];num_rows as usize];
 
     // let player_row:i32 = FromStr::from_str(&iterator.next().unwrap()).unwrap();
     // let player_col:i32 = FromStr::from_str(&iterator.next().unwrap()).unwrap();
@@ -276,19 +259,16 @@ fn main() {
     // let energy_num: usize = FromStr::from_str(&iterator.next().unwrap()).unwrap();
 
 
-    
+    // // Place objects where they are supposed to be on the board
     // let mut object_pos_list = vec![];
-    // while (true) {
+    // loop {
+    //     let next= &iterator.next();
 
-    //     let mut next= (&iterator.next());
-
-    //     if (next.is_some()){
-
+    //     if next.is_some(){
     //         let next_as_usize : usize = FromStr::from_str(next.unwrap()).unwrap();
-
     //         object_pos_list.push(next_as_usize);
             
-    //         if (object_pos_list.len() == 3){
+    //         if object_pos_list.len() == 3 {
     //             let object_kind = object_pos_list.pop().unwrap();
     //             let object_col = object_pos_list.pop().unwrap();
     //             let object_row = object_pos_list.pop().unwrap();
@@ -296,35 +276,32 @@ fn main() {
     //             initial_board[object_row][object_col] = object_kind as i32;
     //             object_pos_list.clear();
     //         }
-
     //     } else {
     //         break;
     //     }
-
-
     // }
 
+    // // Create the board and the map
     // initial_board[player_row as usize][player_col as usize] = 1;
-
     // let mut map = Map{board:initial_board, records: vec![], damage:damage_num,energy: energy_num, row:player_row, col:player_col};
 
     //----------------------------------------------
-    //*****CREATING A MAP THROUGH FILES ENDS*****
+    //    END: CREATING A MAP THROUGH A FILE
     //----------------------------------------------
 
 
   
 
     //----------------------------------------------------------------------
-    // ******CREATING A MAP THROUGH RANDOM BOARD INITIALIZATION STARTS*****
+    //  BEGIN: CREATING A MAP THROUGH RANDOM BOARD INITIALIZATION STARTS
     //-----------------------------------------------------------------------
 
     let mut map = Map{board: vec![vec![0;8];8], records: vec![], damage: 0, energy:10, row:0 as i32, col:0 as i32};
     map.place_elements(20,20,7,7);
 
-     //-----------------------------------------------------------------------
-     // ******CREATING A MAP THROUGH RANDOM BOARD INITIALIZATION ENDS****
-      //-----------------------------------------------------------------------
+    //----------------------------------------------------------------------
+    //  END: CREATING A MAP THROUGH RANDOM BOARD INITIALIZATION STARTS
+    //-----------------------------------------------------------------------
 
 
 
